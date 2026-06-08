848d32fd503be060586d29062b4659195ed78dfe
Heydar Ismayilli <heyderismayilli092@gmail.com>
2026-06-08 19:37:03 +0400

    Added feature to list installed packages

--- src/venv_manager.py
+++ src/venv_manager.py
@@ -12,10 +12,27 @@ def venv_create(venv_name):
     venv_path = pyvenv_path / venv_name  # environment full path
     if os.path.exists(venv_path):
         return False
+
     print(f"'{venv_path}' creating...")
     venv.create(
         env_dir=venv_path,
         with_pip=True
     )
     return True
+
+
+# lists the packages installed on the selected venv environment
+def list_packages(venv_name):
+    venv_path = pyvenv_path / venv_name  # environment full path
+    if not os.path.exists(venv_path):
+        return False
+
+    result = subprocess.run(
+        [venv_path / "bin/pip", "list", "--format", "json"],  # list json format
+        capture_output=True,
+        text=True
+    )
+    return result.stdout
+
+
 
