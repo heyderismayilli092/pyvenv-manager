2f16b864e3a8ba7f915216ee75a12e9cc789625d
Heydar Ismayilli <heyderismayilli092@gmail.com>
2026-06-09 14:06:47 +0400

    The "About" and "Create New Environment" windows have been successfully accessed, and the window switching/closing features are properly integrated

--- src/main.py
+++ src/main.py
@@ -23,12 +23,57 @@ class pyvenv_manager(Gtk.Application):
         builder = Gtk.Builder()
         builder.add_from_file(GLADE_FILE)  # ui path
 
-        win = builder.get_object("main_window")
-        win.set_application(self)
-        win.present()
+        # -------Widget references-------
+        # Main Window
+        self.window = builder.get_object("main_window")
+        self.new_environment_btn = builder.get_object("new_environment")  # create new environment button
+        self.about_btn = builder.get_object("about_button")
 
-    def _on_destroy(self, widget):
-        Gtk.main_quit()
+        # New Environment Window
+        self.new_venv_dialog = builder.get_object("new_venv_dialog")
+        self.cancel_btn = builder.get_object("cancel_btn")
+
+        # About Window
+        self.about_window = builder.get_object("about_window")
+
+        # ----Signals----
+        self.new_environment_btn.connect("clicked", self.on_new_environment)
+        self.about_btn.connect("clicked", self.on_about)
+        self.cancel_btn.connect("clicked", self._on_newvenv_hide)
+
+
+        self.window.set_application(app)
+        self.window.connect("close-request", self._on_destroy)
+        self.window.present()
+
+
+
+    # create new environment
+    def on_new_environment(self, button):
+        self.new_venv_dialog.set_transient_for(self.window)
+        self.new_venv_dialog.set_application(self)
+        self.new_venv_dialog.connect("close-request", self._on_second_close_request)  # pressing the Close (X) key will change "hide" to "destroy"
+        self.new_venv_dialog.present()
+
+    # about window
+    def on_about(self, button):
+        self.about_window.set_transient_for(self.window)
+        self.about_window.set_application(self)
+        self.about_window.connect("close-request", self._on_second_close_request)  # pressing the Close (X) key will change "hide" to "destroy"
+        self.about_window.present()
+    # hide window
+    def _on_second_close_request(self, win):
+        win.hide()
+        return True
+
+    # close environment window
+    def _on_newvenv_hide(self, button):
+        self.new_venv_dialog.hide()
+        return True
+
+    # Main Window Destroy
+    def _on_destroy(self, win):
+        win.destroy()
 
 app = pyvenv_manager()
 app.run()
